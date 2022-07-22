1b6727cd521b97defdebec1f2d0cb17fa6e25ddd
nirjal <nirjalprajapati@gmail.com>
2022-07-22 09:41:31 +0545

    Notice pop up and news done

--- src/App.js
+++ src/App.js
@@ -1,81 +1,37 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import * as React from "react";
 
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import * as React from 'react';
-
-import Carousel from 'react-material-ui-carousel';
-import { Paper} from '@mui/material';
-import Grid from '@mui/material/Grid';
-import TabPanel from './components/SideComponent';
-
-     
-
-function Item(props)
-{
-    return (
-        <Paper >
-          <img
-            width="800" height="500"
-            
-            borderRadius= "30"
-            src={`${props.item.img}?w=800&h=500&fit=crop&auto=format`}
-            srcSet={`${props.item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt={props.item.title}
-            loading="lazy"
-          />
-            <h2>{props.item.title}</h2>
-            <p>{props.item.description}</p>
-        </Paper>
-    )
-}
-
+import Grid from "@mui/material/Grid";
+import TabPanel from "./components/SideComponent";
+import CouroselComponent from "./components/CouroselComponent";
+import Introduction from "./components/Introduction";
 
 function App() {
-  const itemData = [
-    {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'Breakfast',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Burger',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Camera',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      title: 'Coffee',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-      title: 'Hats',
-    }
-]
   return (
     <div className="App">
-      <Header/>
-
-      <Grid container spacing={2} paddingTop={2.5}>
-        <Grid item xs="1"></Grid>
-        <Grid item xs="7">
-          <Carousel
-          fullHeightHover={false} 
-          navButtonsAlwaysVisible={true}
-          >
-              {
-                  itemData.map( (item, i) => <Item key={i} item={item} /> )
-              }
-          </Carousel>
-
+      <Header />
+      <Grid container paddingTop={2.5} direction="column" spacing={10}>
+        <Grid container item spacing={2}>
+          <Grid item xs="1" />
+          <Grid item xs="7">
+            <CouroselComponent />
+          </Grid>
+          <Grid item xs="3">
+            <TabPanel />
+          </Grid>
         </Grid>
-        <Grid item xs="3">
-              <TabPanel/>
+
+        <Grid item container spacing={2}>
+          <Grid item xs={1} />
+          <Grid item xs={10}>
+            <Introduction />
+          </Grid>
+          <Grid item xs={1} />
         </Grid>
       </Grid>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
