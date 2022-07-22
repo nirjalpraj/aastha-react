fe51e7d235fad0b9fce8f18aeb797ba057b8006a
nirjal <nirjalprajapati@gmail.com>
2022-07-22 22:30:56 +0545

    Updates

---
+++ precache-manifest.a2a9f63b54660f6cc087b69302b34b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6ee6afacdc78fb0233",
+    "url": "/static/css/main.be42b514.chunk.css"
+  },
+  {
+    "revision": "ef6ee6afacdc78fb0233",
+    "url": "/static/js/main.dfe56198.chunk.js"
+  },
+  {
+    "revision": "e78a5f6eca927f2bae58",
+    "url": "/static/js/runtime~main.10431c8b.js"
+  },
+  {
+    "revision": "c0ee458290e4ae3de98f",
+    "url": "/static/js/2.c644aef5.chunk.js"
+  },
+  {
+    "revision": "c15fce1f1656145c3459",
+    "url": "/static/js/3.cf58efe1.chunk.js"
+  },
+  {
+    "revision": "3e094e279c6a4471878b00efa6600c01",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/aastha-react/precache-manifest.e3f974e7b2fe650b00a90f8ebdcea721.js"
+  "/precache-manifest.a2a9f63b54660f6cc087b69302b34b44.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/aastha-react/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
